585ef80015c7e1dc5f68d4771622d910ecf67acb
theory friction practice <jackson.brebnor@gmail.com>
2017-02-26 20:22:33 -0500

    find all

--- src/combinations.js
+++ src/combinations.js
@@ -1,8 +1,10 @@
 import { autoPrefix, } from './prefix';
-import { hasSize, sort, } from './utils';
+import { exceeds, hasSize, sort, } from './utils';
 
 export const permute = str => autoPrefix(str);
 export const permLen = len => str => permute(str).filter(hasSize(len));
+export const permMin = (min = 2) => str => permute(str).filter(exceeds(min));
 
 export const combine = str => [ ...new Set(permute(str).map(sort)), ];
 export const combineLen = len => str => [ ...new Set(permLen(len)(str).map(sort)), ];
+export const combineMin = min => str => [ ...new Set(permMin(min)(str).map(sort)), ];
--- src/find.js
+++ src/find.js
@@ -1,5 +1,14 @@
 import anagramica from 'anagramica';
-import { combine, combineLen, } from './combinations';
+import { exceeds, } from './utils';
+import { combine, combineLen,combineMin, } from './combinations';
+
+const best = ({ best, }) => best;
+const all = ({ all, }) => all;
+const map = fn => arr => arr.map(fn);
+const filter = fn => arr => arr.filter(fn);
+const flatBin = (a = [], b = []) => [ ...a, ...b, ];
+const flatten = a => [ ...a, ].reduce(flatBin,[]);
+const hasLength = len => e => e.length === len;
 
 export const find = string =>
  new Promise((resolve, reject) => {
@@ -11,14 +20,10 @@ export const find = string =>
    });
  });
  
-const best = ({ best, }) => best;
-const all = ({ all, }) => all;
-const map = fn => arr => arr.map(fn);
-const filter = fn => arr => arr.filter(fn);
-const flatBin = (a = [], b = []) => [ ...a, ...b, ];
-const flatten = a => [ ...a, ].reduce(flatBin,[]);
-const hasLength = len => e => e.length === len;
-
 export const findByLen = (len = 4) => str =>
-Promise.all(combineLen(len)(str).map(find))
-  .then(flatten).then(filter(hasLength(len)));
+ Promise.all(combineLen(len)(str).map(find))
+   .then(flatten).then(filter(hasLength(len)));
+   
+export const findAll = str =>
+Promise.all(combineMin(2)(str).map(find))
+  .then(flatten).then(filter(exceeds(3)));
