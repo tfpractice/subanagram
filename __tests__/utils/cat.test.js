30d8f72ce8f0eb94d0526866d4dd38ab18e52b03
theory friction practice <jackson.brebnor@gmail.com>
2017-03-03 14:21:47 -0500

    cat test remove extranneous imports

--- __tests__/utils/cat.test.js
+++ __tests__/utils/cat.test.js
@@ -12,41 +12,33 @@ describe('cat', () => {
       expect(catBin('a','b')).toEqual('ab');
     });
   });
-  describe('splat', () => {
-    it('splits a string and concatenates each of its elements', () => {
-      expect(splat('a,b')('x')).toEqual('xa,xb');
-    });
-  });
-  describe('splatBin', () => {
-    it('splits a string and concatenates each of its elements', () => {
-      expect(splatBin('a,b','x')).toEqual('xa,xb');
-    });
-  });
-  describe('repeat', () => {
-    it('repeats a string a given number of times', () => {
-      expect(repeat(2)('ab')).toEqual('abab');
-    });
-  });
-  describe('reps', () => {
-    it('generates an array if orogressive repetitions', () => {
-      expect(reps(3)('ab')).toEqual('ab,abab,ababab');      
-    });
-  });
-  describe('cross', () => {
-    it('combines two strings', () => {
-      // console.log('allSplat',allSplat('abc')('xyz'));
-      // console.log('aa,b,cd,d,aa,ba'.replace(/a/, '_'));
-      console.log('alllb'.replace(/(l*)/, 'l'));
 
-      console.log('cross',cross('abc')('xyz'));
-      console.log('cross2',cross2('abc')('xyz'));
-
-      // console.log('crossBin', crossBin('abc', 'xyz'));
-      // console.log('crossCat',crossCat('abc')('xyz'));
-
-      // console.log('crossCatAll',crossCatAll('abc')('xyz'));
-
-      // console.log('crossCatAll',crossCatAll('a,b,c')('x,y,z,xy, xyz'));
-    });
-  });
+  // describe('splat', () => {
+  //   it('splits a string and concatenates each of its elements', () => {
+  //     expect(splat('a,b')('x')).toEqual('xa,xb');
+  //   });
+  // });
+  // describe('splatBin', () => {
+  //   it('splits a string and concatenates each of its elements', () => {
+  //     expect(splatBin('a,b','x')).toEqual('xa,xb');
+  //   });
+  // });
+  // describe('repeat', () => {
+  //   it('repeats a string a given number of times', () => {
+  //     expect(repeat(2)('ab')).toEqual('abab');
+  //   });
+  // });
+  // describe('reps', () => {
+  //   it('generates an array if orogressive repetitions', () => {
+  //     expect(reps(3)('ab')).toEqual('ab,abab,ababab');      
+  //   });
+  // });
+  // describe('cross', () => {
+  //   it('combines two strings', () => {
+  //     console.log('alllb'.replace(/(l*)/, 'l'));
+  // 
+  //     console.log('cross',cross('abc')('xyz'));
+  //     console.log('cross2',cross2('abc')('xyz'));
+  //   });
+  // });
 });
